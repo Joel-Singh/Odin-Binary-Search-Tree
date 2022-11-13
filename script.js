6b136a2355b23c6a6e3dfb05f9c1f46306797565
Joel Singh <joelsingh788@gmail.com>
2022-11-13 16:24:04 -0500

    Add prettyPrint function

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ function createNode(value, left = null, right = null) {
     right,
   };
 }
+
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+  }
+  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.value}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+  }
+}
