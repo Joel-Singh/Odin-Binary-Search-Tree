04fd9c02de8f4618cecbeaf47f2f9c429304ac51
Joel Singh <joelsingh788@gmail.com>
2022-11-13 16:24:13 -0500

    Add createTree function

--- script.js
+++ script.js
@@ -6,6 +6,31 @@ function createNode(value, left = null, right = null) {
   };
 }
 
+function createTree(array) {
+  let newTree = {
+    root: null,
+    buildTree: function (array, start = 0, end = array.length - 1) {
+      array.sort((x, y) => x - y);
+      // Removes duplicates
+      array = [...new Set(array)]
+
+      if (start === end) {
+        return createNode(array[0]);
+      }
+
+      let midPoint = Number.parseInt((end + start) / 2);
+
+      return createNode(
+        array[midPoint],
+        this.buildTree(array, start, midPoint),
+        this.buildTree(array, midPoint + 1, end)
+      );
+    },
+  };
+  newTree.root = newTree.buildTree(array);
+  return newTree
+}
+
 const prettyPrint = (node, prefix = '', isLeft = true) => {
   if (node.right !== null) {
     prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
